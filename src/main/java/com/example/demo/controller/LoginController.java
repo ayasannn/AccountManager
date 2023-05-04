42955737a54418265c7df65a51ae8c64bf9fbd0e
kusamochi25 <quadr1_phm@icloud.com>
2023-05-04 15:10:38 +0900

    頑張ったけど相関項目チェックがうまくいかなかった。古川さん頼みたい。

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.InsertForm;
+import com.example.demo.validator.MultiValidator;
 
 @Controller
 public class LoginController {
@@ -15,16 +19,25 @@ public class LoginController {
 	public InsertForm setUpForm() {
 		return new InsertForm();
 	}
+	
 	@GetMapping("AccountManager")
 	public String showView() {
 		return "login";
 	}
+	
 	@PostMapping("home")
 	public String loginView(@Validated InsertForm form, BindingResult bindingResult) {
 		if(bindingResult.hasErrors()) {
 			return "login";
 		}
 		return "home";
+	}
+	
+	@Autowired
+	MultiValidator mv;
+	@InitBinder("insertForm")
+	public void initBinder(WebDataBinder wdb) {
+		wdb.addValidators(mv);
 	}
 }
 
--- src/main/java/com/example/demo/form/InsertForm.java
+++ src/main/java/com/example/demo/form/InsertForm.java
@@ -1,18 +1,21 @@
 package com.example.demo.form;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class InsertForm {
 
-//	@MailValid
-	@NotBlank
+	@Pattern(regexp="^([\\w])+([\\w\\._\\-])*@([\\w])+([\\w\\._-])*\\.([a-zA-Z])+$",
+			message = "正しいメールアドレスを入力してください。")
+	@NotBlank(message = "未入力です。")//OK
 	private String mail;
 	
-//	@Password
-	@NotBlank
-	@NotNull
+	@Pattern(regexp="^([\\w\\._\\-\\+])*$",
+			message ="入力できない文字があります。")
+	@NotBlank(message = "未入力です。")//OK
+	@Size(min = 6, max = 12, message = "パスワードは6～12文字です。")
 	private String pass;
 }
