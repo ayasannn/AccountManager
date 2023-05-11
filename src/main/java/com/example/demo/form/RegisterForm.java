a2749f8ab5e125591f4189b9176383c76201d5c7
kusamochi25 <quadr1_phm@icloud.com>
2023-05-11 11:25:36 +0900

    バリデーションいじった。

--- src/main/java/com/example/demo/form/RegisterForm.java
+++ src/main/java/com/example/demo/form/RegisterForm.java
@@ -7,6 +7,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -14,17 +16,19 @@ import lombok.Data;
 @Data
 public class RegisterForm{
 	
-	@Pattern(regexp="^([\\w])+([\\w\\._\\-])*@([\\w])+([\\w\\._-])*\\.([a-zA-Z])+$",
+	@Pattern(regexp ="^([\\w])+([\\w\\._\\-])*@([\\w])+([\\w\\._-])*\\.([a-zA-Z])+$",
 			message = "正しいメールアドレスを入力してください。")
 	@NotBlank(message = "メールアドレスが未入力です。")//OK
 	private String mail;
 	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	@JsonFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@Past(message = "過去の日付を選択してください。")
+	@NotNull(message = "生年月日を入力してください。")
 	private Date birth;
 	
-	@Pattern(regexp="^([\\w\\._\\-\\+])*$",
-			message ="入力できない文字があります。")
+	@Pattern(regexp = "^([\\w\\._\\-\\+])*$",
+			message = "入力できない文字があります。")
 	@NotBlank(message = "パスワードが未入力です。")//OK
 	@Size(min = 6, max = 12, message = "パスワードは6～12文字です。")
 	private String pass;
