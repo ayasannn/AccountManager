01c15fcb0ceb44b3f9e8c190436b81e796f75466
ayasannn <monsan04510@gmail.com>
2023-05-12 14:15:23 +0900

    謎修正

--- src/main/java/com/example/demo/controller/RenewalController.java
+++ src/main/java/com/example/demo/controller/RenewalController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.form.RenewalForm;
 import com.example.demo.repository.Accounts;
@@ -24,7 +24,7 @@ public class RenewalController {
 		return new RenewalForm();
 	}
 	
-	@RequestMapping("renewal")
+	@GetMapping("renewal")
 	public String showView() {
 		return "renewal";
 	}
